66d74ccbd4ddfa783b4996c094f6a7d6d1a2e637
Tapiwa Motsi <tmotsi14@gmail.com>
2022-11-20 19:39:05 +0200

    Nav bar added

--- src/views/AccountView.vue
+++ src/views/AccountView.vue
@@ -1,10 +1,13 @@
 <template>
+    <TopNavigation/>
     <div class="max-w-4xl mx-auto" id="AccountView">
         <router-view/>
     </div>
  
 </template>
-
+<script setup>
+import TopNavigation from '@/components/structure/TopNavigation.vue';
+</script>
 <style lang="scss">
 
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,13 @@
 <template>
   <div>
+    <TopNavigation/>
     <HeroSection/>
   </div>
 </template>
 
 <script setup>
 import HeroSection from '@/components/partials/HeroSection.vue';
-
+// import TopNavigationVue from '@/components/structure/TopNavigation.vue';
+import TopNavigation from '@/components/structure/TopNavigation.vue';
 
 </script>
