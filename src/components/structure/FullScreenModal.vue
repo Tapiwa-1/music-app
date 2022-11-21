7241b647cc6111f2cd0aafca5d2c50b5d5498755
Tapiwa Motsi <tmotsi14@gmail.com>
2022-11-21 07:10:03 +0200

    Fixing modal

--- src/components/structure/FullScreenModal.vue
+++ src/components/structure/FullScreenModal.vue
@@ -1,20 +1,49 @@
 <template>
-  <div class="">
-    <button @click="open =!open" class="bg-transparent hover:bg-green-400 text-white font-white font-semibold hover:text-white py-1 px-2 hover:border-transparent rounded">
-        <i class="fa-solid fa-bars fa-xl"></i>
-    </button>
-    <div v-show="open" class="animated slideInDown faster fixed w-full h-full top-0 left-0 flex justify-center backdrop-blur-sm">
-        <div class="bg-black absolute w-full h-full opacity-75"></div>
-        <div class="my-auto fixed border-white w-80 pt-16">
-            <p class="text-2xl text-center text-white font-bold">Menu</p>
-        </div>
-        <div class="pt-2">
-            <RouterLinkButton class="w-full text-gray-100 text-center text-lg" btnText="Profile" color="red" url="/account/profile"/>
-        </div>
-    </div>
-  </div>
-</template>
+    <div>
+    
+        <button 
+            @click="open = !open"
+            class="
+                bg-transparent 
+                hover:bg-green-400 
+                text-white 
+                font-semibold 
+                hover:text-white
+                py-1
+                px-2
+                hover:border-transparent
+                rounded
+            "
+        >
+            <i class="fa-solid fa-bars fa-xl"></i>
+        </button>
 
+        <div 
+            v-show="open"
+            class="
+                animated 
+                slideInDown 
+                faster 
+                fixed 
+                w-full 
+                h-full 
+                top-0 
+                left-0
+                flex
+                justify-center
+                backdrop-blur-sm
+            "
+        >
+            <div class="bg-black absolute w-full h-full opacity-75"></div>
+            <div class="my-auto fixed border-white w-80 pt-16">
+                <p class="text-2xl text-center text-white font-bold">Menu</p>
+                <RouterLinkButton class="w-full text-gray-100 text-center text-lg" btnText="Profile" color="indigo" url="/account/profile"/>
+
+            </div>
+        </div>
+
+    </div>
+</template>
 <script setup>
 import { ref } from 'vue';
 import RouterLinkButton from '@/components/global/RouterLinkButton.vue'
